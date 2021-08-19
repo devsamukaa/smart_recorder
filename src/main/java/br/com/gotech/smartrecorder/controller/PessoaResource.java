d69f02450590db14ca619e0f52b1d7ed9e8e8039
meli-devsamuka <samuel.rocha@mercadolivre.com>
2021-08-19 15:26:43 -0300

    remove autowired

--- src/main/java/br/com/gotech/smartrecorder/controller/PessoaResource.java
+++ src/main/java/br/com/gotech/smartrecorder/controller/PessoaResource.java
@@ -17,8 +17,6 @@ import java.util.List;
 public class PessoaResource {
     @Autowired
     private PessoaRepository pessoaRepository;
-
-    @Autowired
     PessoaRepositoryImpl pessoaRepositoryImpl;
 
     @GetMapping
